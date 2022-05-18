3d0001ec57c2bb8e1e86a54ae5f0332c75646637
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-05-18 14:42:29 -0300

    feat: add route - get by talker id

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const data = require('../talker.json');
+const { verifyDataContent } = require('../middlewares/index');
 
 const routes = express.Router();
 
@@ -9,6 +10,15 @@ const HTTP_NOT_FOUND_STATUS = 404;
 routes.get('/talker', (req, res) => {
   try {
     res.status(HTTP_OK_STATUS).json(data);
+  } catch (err) {
+    res.status(HTTP_NOT_FOUND_STATUS).json({ err });
+  }
+});
+
+routes.get('/talker/:id', verifyDataContent, (req, res) => {
+  try {
+    const selectedTalker = data.find((talker) => +talker.id === +req.params.id);
+    res.status(HTTP_OK_STATUS).json(selectedTalker);
   } catch (err) {
     res.status(HTTP_NOT_FOUND_STATUS).json({ err });
   }
