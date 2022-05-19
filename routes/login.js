6913077b1a735d31962cf72edd9d88606c624a82
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-05-18 21:35:59 -0300

    feat: add try..catch structure to login route with auth

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const crypto = require('crypto');
+const { auth } = require('../middlewares/index');
 const { HTTP_OK_STATUS } = require('../httpStatusCodes');
 
 const routes = express.Router();
 
-routes.post('/login', (req, res) => {
-  const token = crypto.randomBytes(8).toString('hex');
-  res.status(HTTP_OK_STATUS).json({ token });
+routes.post('/login', auth, (req, res) => {
+  try {
+    const token = crypto.randomBytes(8).toString('hex');
+    return res.status(HTTP_OK_STATUS).json({ token });
+  } catch (err) {
+    console.log('Erro:', err);
+  }
 });
 
 module.exports = routes;
