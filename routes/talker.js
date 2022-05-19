4f64722d86554dfb874842428cd77b8fb94858dd
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-05-19 15:16:12 -0300

    feat: add POST talker route with placeholder success message

--- routes/talker.js
+++ routes/talker.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const { readFileSync } = require('fs');
-
-const { HTTP_OK_STATUS, HTTP_NOT_FOUND_STATUS } = require('../httpStatusCodes');
+const { checkNewTalkerInfo } = require('../middlewares/index');
+const {
+  HTTP_OK_STATUS,
+  HTTP_NOT_FOUND_STATUS,
+  HTTP_CREATED_STATUS,
+} = require('../httpStatusCodes');
 
 const routes = express.Router();
 
@@ -14,13 +18,17 @@ routes.get('/talker/:id', (req, res) => {
   const { id } = req.params;
   const talkers = JSON.parse(readFileSync('talker.json', 'utf-8'));
   const chosenTalker = talkers.find((talker) => +talker.id === +id);
-  
+
   if (!chosenTalker) {
     res
       .status(HTTP_NOT_FOUND_STATUS)
       .json({ message: 'Pessoa palestrante não encontrada' });
   }
   res.status(HTTP_OK_STATUS).json(chosenTalker);
+});
+
+routes.post('/talker', checkNewTalkerInfo, (req, res) => {
+  res.status(HTTP_CREATED_STATUS).json({ message: 'Palestrante adicionado' });
 });
 
 module.exports = routes;
