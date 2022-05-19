5bb98ab87e8b2bfe0f511c7d7f0270f0a894c3df
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-05-19 15:17:18 -0300

    feat: add new checkNewTalkerInfo middleware to be used inf talker POST route

---
+++ middlewares/checkNewTalkerInfo.js
@@ -0,0 +1,75 @@
+const { HTTP_BAD_RESQUEST_STATUS } = require('../httpStatusCodes');
+
+const checkName = (res, name) => {
+  if (name === undefined) {
+    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+      message: 'O campo "name" é obrigatório',
+    });
+  }
+  if (name.length < 3) {
+    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+};
+
+const checkAge = (res, age) => {
+  if (!age) {
+    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+      message: 'O campo "age" é obrigatório',
+    });
+  }
+  if (age < 18) {
+    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+      message: 'A pessoa palestrante deve ser maior de idade',
+    });
+  }
+};
+
+const checkWatch = (res, watchedAt) => {
+  const DATA_FORMAT = new RegExp(/[0-31]{2,2}\/[0-12]{2,2}\/[0-9999]{4,4}/);
+
+  if (!DATA_FORMAT.test(watchedAt)) {
+    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+};
+
+const checkRate = (res, rate) => {
+  if (rate < 1 || rate > 5) {
+    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+};
+
+const checkTalk = (res, talk) => {
+  if (!talk) {
+    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+      message:
+        'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+  const { watchedAt, rate } = talk;
+
+  if (!watchedAt || !rate) {
+    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+      message:
+        'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+  checkRate(res, rate);
+  checkWatch(res, watchedAt);
+};
+
+const checkNewTalkerInfo = (req, res, next) => {
+  const { name, age, talk } = req.body;
+  checkName(res, name);
+  checkAge(res, age);
+  checkTalk(res, talk);
+
+  next();
+};
+
+module.exports = checkNewTalkerInfo;
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const error = require('./error');
 const hasCredentials = require('./hasCredentials');
+const checkNewTalkerInfo = require('./checkNewTalkerInfo');
 
 module.exports = {
   error,
   hasCredentials,
+  checkNewTalkerInfo,
 };
