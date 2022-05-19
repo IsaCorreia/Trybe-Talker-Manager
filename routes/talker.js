34c528c6f797a6b24239644e6312bbd9a0fa9d56
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-05-18 21:33:07 -0300

    feat: read file through fs module

--- routes/talker.js
+++ routes/talker.js
@@ -1,27 +1,13 @@
 const express = require('express');
-const data = require('../talker.json');
-const { verifyDataContent } = require('../middlewares/index');
+const { readFileSync } = require('fs');
+
+const { HTTP_OK_STATUS } = require('../httpStatusCodes');
 
 const routes = express.Router();
 
-const HTTP_OK_STATUS = 200;
-const HTTP_NOT_FOUND_STATUS = 404;
-
 routes.get('/talker', (req, res) => {
-  try {
-    res.status(HTTP_OK_STATUS).json(data);
-  } catch (err) {
-    res.status(HTTP_NOT_FOUND_STATUS).json({ err });
-  }
-});
-
-routes.get('/talker/:id', verifyDataContent, (req, res) => {
-  try {
-    const selectedTalker = data.find((talker) => +talker.id === +req.params.id);
-    res.status(HTTP_OK_STATUS).json(selectedTalker);
-  } catch (err) {
-    res.status(HTTP_NOT_FOUND_STATUS).json({ err });
-  }
+  const data = JSON.parse(readFileSync('talker.json', 'utf-8'));
+  res.status(HTTP_OK_STATUS).json(data);
 });
 
 module.exports = routes;
