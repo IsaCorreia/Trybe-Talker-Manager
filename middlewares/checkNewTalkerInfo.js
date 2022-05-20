40160489c2ed3a4693a0c087c06aa039e683902f
IsaCorreia <isabela.correiasilva00@gmail.com>
2022-05-20 20:25:01 -0300

    fix: replaces RESQUEST typo to REQUEST

--- middlewares/checkNewTalkerInfo.js
+++ middlewares/checkNewTalkerInfo.js
@@ -1,13 +1,13 @@
-const { HTTP_BAD_RESQUEST_STATUS } = require('../httpStatusCodes');
+const { HTTP_BAD_REQUEST_STATUS } = require('../httpStatusCodes');
 
 const checkName = (res, name) => {
   if (name === undefined) {
-    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
       message: 'O campo "name" é obrigatório',
     });
   }
   if (name.length < 3) {
-    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
       message: 'O "name" deve ter pelo menos 3 caracteres',
     });
   }
@@ -15,12 +15,12 @@ const checkName = (res, name) => {
 
 const checkAge = (res, age) => {
   if (!age) {
-    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
       message: 'O campo "age" é obrigatório',
     });
   }
   if (age < 18) {
-    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
       message: 'A pessoa palestrante deve ser maior de idade',
     });
   }
@@ -30,7 +30,7 @@ const checkWatch = (res, watchedAt) => {
   const DATA_FORMAT = new RegExp(/[0-31]{2,2}\/[0-12]{2,2}\/[0-9999]{4,4}/);
 
   if (!DATA_FORMAT.test(watchedAt)) {
-    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
       message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
     });
   }
@@ -38,7 +38,7 @@ const checkWatch = (res, watchedAt) => {
 
 const checkRate = (res, rate) => {
   if (rate < 1 || rate > 5) {
-    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
       message: 'O campo "rate" deve ser um inteiro de 1 à 5',
     });
   }
@@ -46,7 +46,7 @@ const checkRate = (res, rate) => {
 
 const checkTalk = (res, talk) => {
   if (!talk) {
-    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
       message:
         'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
@@ -54,7 +54,7 @@ const checkTalk = (res, talk) => {
   const { watchedAt, rate } = talk;
 
   if (!watchedAt || !rate) {
-    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
       message:
         'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
--- middlewares/hasCredentials.js
+++ middlewares/hasCredentials.js
@@ -1,16 +1,16 @@
-const { HTTP_BAD_RESQUEST_STATUS } = require('../httpStatusCodes');
+const { HTTP_BAD_REQUEST_STATUS } = require('../httpStatusCodes');
 
 const validateCredentials = (res, email, password) => {
   const validEmail = new RegExp(/@./);
   const validPassword = new RegExp(/....../);
 
   if (!validEmail.test(email)) {
-    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
       message: 'O "email" deve ter o formato "email@email.com"',
     });
   }
   if (!validPassword.test(password)) {
-    return res.status(HTTP_BAD_RESQUEST_STATUS).json({
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
       message: 'O "password" deve ter pelo menos 6 caracteres',
     });
   }
@@ -20,12 +20,12 @@ const hasCredentials = (req, res, next) => {
   const { email, password } = req.body;
   if (!email) {
     return res
-      .status(HTTP_BAD_RESQUEST_STATUS)
+      .status(HTTP_BAD_REQUEST_STATUS)
       .json({ message: 'O campo "email" é obrigatório' });
   }
   if (!password) {
     return res
-      .status(HTTP_BAD_RESQUEST_STATUS)
+      .status(HTTP_BAD_REQUEST_STATUS)
       .json({ message: 'O campo "password" é obrigatório' });
   }
   validateCredentials(res, email, password);
