d826f5ebc65284dd7edb58d440a7eb4ddfeffb4c
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-05-21 19:20:12 -0300

    refactor: changes from middleware to utils
    deleteData is now a functions instead of a middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,12 +2,10 @@ const error = require('./error');
 const hasCredentials = require('./hasCredentials');
 const checkNewTalkerInfo = require('./checkNewTalkerInfo');
 const tokenAuth = require('./tokenAuth');
-const deleteData = require('./deleteData');
 
 module.exports = {
   error,
   hasCredentials,
   checkNewTalkerInfo,
   tokenAuth,
-  deleteData,
 };
---
+++ utils/deleteData.js
@@ -0,0 +1,16 @@
+const { readFileSync } = require('fs');
+const writeData = require('./writeData');
+
+const deleteData = (id) => {
+  try {
+    const data = readFileSync('talker.json', 'utf-8');
+    const talkersObj = JSON.parse(data);
+    const filteredTalkers = talkersObj.filter((talker) => +talker.id !== +id);
+    const filteredTalkersSting = JSON.stringify(filteredTalkers);
+    writeData(filteredTalkersSting);
+  } catch (err) {
+    console.log({ Erro_ao_deletar: err.message });
+  }
+};
+
+module.exports = deleteData;
