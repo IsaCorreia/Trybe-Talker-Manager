de10063fa45f728add760d7664b38f06c5fef6ad
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-05-21 20:37:16 -0300

    refactor: add conditional to error middleware

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,7 +1,7 @@
 const { HTTP_INTERNAL_ERROR_STATUS } = require('../httpStatusCodes');
 
 const error = (err, req, res, next) => {
-  res.status(err.status || HTTP_INTERNAL_ERROR_STATUS).json({ error: err.message });
+  if (err) return res.status(err.status || HTTP_INTERNAL_ERROR_STATUS).json({ error: err.message });
   next();
 };
 
