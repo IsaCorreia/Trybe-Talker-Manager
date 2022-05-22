58d86309101899347b5e2f475db25454934bbd84
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-05-22 12:16:45 -0300

    fix: changes to error treatment in middlewares
    error.js: added console log for better visualization.
    tokenAuth.js: removed try/catch structure from middleware.

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,7 +1,13 @@
 const { HTTP_INTERNAL_ERROR_STATUS } = require('../httpStatusCodes');
 
 const error = (err, req, res, next) => {
-  if (err) return res.status(err.status || HTTP_INTERNAL_ERROR_STATUS).json({ error: err.message });
+  if (err) {
+    console.log({
+      status: err.status || HTTP_INTERNAL_ERROR_STATUS,
+      error: err.message,
+    });
+    return res.status(err.status || HTTP_INTERNAL_ERROR_STATUS).json({ error: err.message });
+  }
   next();
 };
 
--- middlewares/tokenAuth.js
+++ middlewares/tokenAuth.js
@@ -1,25 +1,19 @@
-const {
-  HTTP_UNAUTHORIZED_STATUS,
-  HTTP_INTERNAL_ERROR_STATUS,
-} = require('../httpStatusCodes');
+const { HTTP_UNAUTHORIZED_STATUS } = require('../httpStatusCodes');
 
 const tokenAuth = (req, res, next) => {
   const { authorization } = req.headers;
   const TOKEN_FORMAT = /[^&]{16,16}/;
-  try {
-    if (!authorization) {
-      return res.status(HTTP_UNAUTHORIZED_STATUS).json({
-        message: 'Token não encontrado',
-      });
-    }
-    if (!TOKEN_FORMAT.test(authorization)) {
-      return res.status(HTTP_UNAUTHORIZED_STATUS).json({
-        message: 'Token inválido',
-      });
-    }
-  } catch (err) {
-    return res.status(HTTP_INTERNAL_ERROR_STATUS).json({ Erro_ao_autenticar: err.message });
+  if (!authorization) {
+    return res.status(HTTP_UNAUTHORIZED_STATUS).json({
+      message: 'Token não encontrado',
+    });
   }
+  if (!TOKEN_FORMAT.test(authorization)) {
+    return res.status(HTTP_UNAUTHORIZED_STATUS).json({
+      message: 'Token inválido',
+    });
+  }
+
   next();
 };
 
